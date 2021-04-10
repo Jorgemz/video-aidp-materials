59a6a2c0c172988b01d35a0c3c557fbba2241ae1
🤨 <jorge_mz@icloud.com>
2021-04-10 11:30:38 -0500

    - pattern flyweight, can you build store variable(static) into a extension class minimize create many variables by valor equatable

--- 02-flyweight/starter/AdvancedDesignPatterns.xcworkspace/AdvancedDesignPatterns.playground/Pages/Flyweight.xcplaygroundpage/Contents.swift
+++ 02-flyweight/starter/AdvancedDesignPatterns.xcworkspace/AdvancedDesignPatterns.playground/Pages/Flyweight.xcplaygroundpage/Contents.swift
@@ -1,3 +1,4 @@
+
 /*:
  [Previous](@previous)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;[Next](@next)
  
@@ -11,3 +12,29 @@
  
  ## Code Example
  */
+import UIKit
+import os
+
+let logger = Logger(subsystem: "com.dserweb.flyweight", category: "flyweight")
+
+let red = UIColor.red
+let red2 = UIColor.red
+print(red === red2)
+
+extension UIColor {
+  public static var colorStore: [String: UIColor] = [:]
+  public class func rgba(_ red: CGFloat,
+                         _ green: CGFloat,
+                         _ blue: CGFloat,
+                         _ alpha: CGFloat) -> UIColor {
+    let key = "\(red)\(green)\(alpha)"
+    if let color = colorStore[key] { return color }
+    let color = UIColor(red: red, green: green, blue: blue, alpha: alpha)
+    colorStore[key] = color
+    return color
+  }
+}
+
+let color1 = UIColor.rgba(1, 0, 0, 1)
+let color2 = UIColor.rgba(1, 0, 0, 1)
+print(color1 === color2)
